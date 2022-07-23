c646534bad7459ba30eb1e300e5cf38465183b0e
shahin <shahin.shahi1986@gmail.com>
2022-07-23 19:02:22 +0300

    call api is sucsses

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import HomeHeader from "./HomeHeader";
 import NFTCards from "./NFTCards";
-import FocuseStatusBar from "./FocusedStatusBar";
+import FocusedStatusBar from "./FocusedStatusBar";
 
 export {
-    HomeHeader,NFTCards,FocuseStatusBar
+    HomeHeader,NFTCards,FocusedStatusBar
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import { Text,SafeAreaView,View,FlatList } from 'react-native'
 import { COLORS, NFTData } from '../constants'
-import {HomeHeader,NFTCards,FocuseStatusBar} from '../components'
+import {HomeHeader,NFTCards,FocusedStatusBar} from '../components'
 
-function Home() {
+function Home(props) {
 
 
     return (
         <SafeAreaView style={{flex:1}}>
-            <FocuseStatusBar background={COLORS.primary } />
-            {/* <NFTCards />
-            <HomeHeader/> */}
+            <FocusedStatusBar background={COLORS.primary } />
+             <View style={{ flex: 1 }}>
+              <View style={{ zIndex: 0 }}>
+                 <FlatList
+                    data={NFTData}
+                    renderItem={({ item }) => <Text>{item.name}</Text> }
+                    keyExtractor={(item) => item.id}
+                    showsVerticalScrollIndicator={false}
+                    ListHeaderComponent={<HomeHeader  />}
+                    />
+              </View>
+            </View>
       </SafeAreaView>
    
   )
