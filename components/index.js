0a78881e18eaf961d32b616b72b1e49e9b0c6cea
shahin <shahin.shahi1986@gmail.com>
2022-07-24 10:58:13 +0300

    detailes page is ready

--- components/index.js
+++ components/index.js
@@ -1,12 +1,14 @@
 import HomeHeader from "./HomeHeader";
 import NFTCards from "./NFTCards";
 import FocusedStatusBar from "./FocusedStatusBar";
-import { CircleButton,RectButton } from "./Button";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 
 export {
     HomeHeader,
     NFTCards,
     FocusedStatusBar,
     CircleButton,
-    RectButton
+    RectButton,DetailsBid,DetailsDesc
 }
