8008c14432048d335795cb20f5431ed61005a662
shahin <shahin.shahi1986@gmail.com>
2022-07-24 11:21:31 +0300

    detiles route is ready

--- components/index.js
+++ components/index.js
@@ -4,11 +4,11 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
-
+import { SubInfo } from "./SubInfo";
 export {
     HomeHeader,
     NFTCards,
     FocusedStatusBar,
     CircleButton,
-    RectButton,DetailsBid,DetailsDesc
+    RectButton,DetailsBid,DetailsDesc,SubInfo
 }
