225f75a4e76225d55787bf9a3df23e5e73118eec
shahin <shahin.shahi1986@gmail.com>
2022-07-24 09:25:49 +0300

    make nft card

--- components/index.js
+++ components/index.js
@@ -1,7 +1,12 @@
 import HomeHeader from "./HomeHeader";
 import NFTCards from "./NFTCards";
 import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton,RectButton } from "./Button";
 
 export {
-    HomeHeader,NFTCards,FocusedStatusBar
+    HomeHeader,
+    NFTCards,
+    FocusedStatusBar,
+    CircleButton,
+    RectButton
 }
